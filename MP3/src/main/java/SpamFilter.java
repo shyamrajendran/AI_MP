9f4cdd4b7a9ba465d50fc66c3f835e095a3ce8ce
Shyam Rajendran <rshyam.psg@gmail.com>
2015-04-14 02:30:13 -0500

    report add and bag of word

---
+++ MP3/src/main/java/SpamFilter.java
@@ -0,0 +1,322 @@
+import com.sun.org.apache.xpath.internal.operations.Bool;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.text.DecimalFormat;
+import java.util.*;
+
+
+/**
+ * Created by sam on 4/7/15.
+ */
+public class SpamFilter {
+    //
+    // DO NOT USE THIS CLASS.
+    //
+    //
+    // DO NOT USE THIS CLASS.
+    //
+    //
+    // DO NOT USE THIS CLASS.
+    //
+    //
+    // DO NOT USE THIS CLASS.
+    //
+
+    //
+    // DO NOT USE THIS CLASS.
+    //
+    //
+    // DO NOT USE THIS CLASS.
+    //
+    //
+    // DO NOT USE THIS CLASS.
+    //
+    //
+    // DO NOT USE THIS CLASS.
+    //
+
+    final private double LAPLACE = 1.0;
+    private int spam_messages = 0;
+    private int non_spam_message = 0;
+    private ArrayList<String[]> spam_words;
+    private HashMap<String, Integer> spam_hash = new HashMap<String, Integer>();
+    private HashMap<String, Integer> non_spam_hash = new HashMap<String, Integer>();
+    private HashMap<String, Double> spamCondProb = new HashMap<String, Double>();
+    private HashMap<String, Double> nonSpamCondProb = new HashMap<String, Double>();
+    private Set<String> totalUniqueWords = new HashSet<String>();
+    private HashMap<String , ArrayList<Double>> confusionMatrix = new HashMap<String , ArrayList<Double>>(2);
+
+    private int uniqSpamWords;
+    private int uniqNonSpamWords;
+    private int totalSpamWords;
+    private int totalNonSpamWords;
+    private int testDocumentSpamCount = 0 ;
+    private int testDocumentNonSpamCount = 0;
+    private double spamProb ;
+
+
+    public SpamFilter(String fileName) throws IOException {
+        readFile(fileName);
+        spamProb = calcSpamProb(spam_messages, non_spam_message);
+        spamCondProb = calcCondProb(spam_hash);
+        nonSpamCondProb = calcCondProb(non_spam_hash);
+    }
+
+
+    private int calcTotalWords(HashMap<String, Integer> ha){
+        int totalWords = 0 ;
+        for(Map.Entry<String, Integer> entry : ha.entrySet()) {
+            totalWords+=entry.getValue();
+        }
+        return totalWords;
+    }
+    private HashMap<String, Double> calcCondProb(HashMap<String, Integer> ha) {
+        HashMap<String, Double> res = new HashMap<String, Double>();
+        double tempWordProb = 0.0;
+        int totalWords = calcTotalWords(ha);
+        // get total number of unique words to add here.
+        totalWords+=totalUniqueWords.size();
+        for(Map.Entry<String, Integer> entry : ha.entrySet()) {
+            tempWordProb = (double ) (entry.getValue()+ LAPLACE)  / totalWords;
+            res.put(entry.getKey(), tempWordProb);
+        }
+        return res;
+    }
+
+
+    private double calcMessProb(String[] message, Boolean type){
+        int totalWords;
+        String[] keyValuePair = new String[2];
+        int val;
+        double totalProduct ;
+        double wordProb = 0.0;
+        String key;
+        if ( type ) {
+            totalWords = calcTotalWords(spam_hash);
+            totalProduct = Math.log(spamProb);
+        } else {
+            totalWords = calcTotalWords(non_spam_hash);
+            totalProduct = Math.log(1 - spamProb);
+        }
+
+
+        for (String st : message){
+            keyValuePair= st.split(":");
+            key = keyValuePair[0];
+            val = Integer.parseInt(keyValuePair[1]);
+            if (! totalUniqueWords.contains(key)) continue;
+            // this is as per post https://piazza.com/class/i56vzaj3akl4wf?cid=398
+            // “create dictionaries consisting of all unique words occurring in the training documents”
+            // Apply Laplace smoothing to the words in the dictionary (some words may not appear in certain classes).
+            // For words in the test documents, which doesn't appear in the dictionary, ignore them.
+
+            if (type){
+                // indicates spam message has been read. Now check what your classification predicts
+                if (!spamCondProb.containsKey(key)){
+                    wordProb = LAPLACE / ( totalWords + totalUniqueWords.size());
+                } else {
+                    wordProb = spamCondProb.get(key);
+                }
+
+            } else {
+                if (!nonSpamCondProb.containsKey(key)){
+                    wordProb = LAPLACE / ( totalWords + totalUniqueWords.size());
+                } else {
+                    wordProb = nonSpamCondProb.get(key);
+                }
+            }
+            totalProduct += Math.log(wordProb) * val;
+
+        }
+        return totalProduct;
+    }
+
+    private void printConfusionMatrix(){
+        DecimalFormat df = new DecimalFormat("#.00");
+
+        System.out.println("TEST SPAM DOCUMENTS READ     :" +testDocumentSpamCount);
+        System.out.println("TEST NON SPAM DOCUMENTS READ :" +testDocumentNonSpamCount);
+        System.out.println("CONFUSION MATRIX");
+        System.out.println("***** PREDICTION SCORES ********* ");
+        System.out.println("    SPAM    NONSPAM");
+        System.out.print("SPAM  ");
+
+        for (Double d : confusionMatrix.get("spam")){
+            System.out.print(df.format(d/testDocumentSpamCount*100)+"%" + " ");
+        }
+        System.out.println();
+        System.out.print("NONSPAM ");
+        for (Double d : confusionMatrix.get("nonSpam")){
+            System.out.print(df.format(d/testDocumentNonSpamCount*100)+"%" + " ");
+        }
+    }
+    private void predictClassification(String fileName) throws IOException {
+        BufferedReader  bufferedReader = new BufferedReader(new FileReader(fileName));
+        String in_line;
+        String[] temp ;
+        double messageSpam ;
+        String test;
+        String prediction;
+        double messageNonSpam;
+        double res;
+
+        while((in_line = bufferedReader.readLine()) != null){
+            ArrayList<Double> matrixDetails = new ArrayList<Double>(2);
+            if (in_line.equals("")) continue;
+            temp = in_line.split(" ");
+            if ( temp[0].equals("1") ) {
+                test = "spam";
+                testDocumentSpamCount++;
+            } else {
+                test = "nonSpam";
+                testDocumentNonSpamCount++;
+            }
+
+
+            messageSpam = calcMessProb(Arrays.copyOfRange(temp, 1, temp.length), Boolean.TRUE);
+            messageNonSpam = calcMessProb(Arrays.copyOfRange(temp, 1, temp.length), Boolean.FALSE);
+
+            if (messageSpam > messageNonSpam) {
+                prediction = "spam";
+            } else {
+                prediction = "nonSpam";
+            }
+
+            double t0;
+            double t1;
+            if (test.equals(prediction)){
+                if (test.equals("spam")){
+                    // spam spam
+                    if (! confusionMatrix.containsKey("spam")) {
+                        matrixDetails.add(1.0);
+                        matrixDetails.add(0.0);
+                        confusionMatrix.put("spam",matrixDetails);
+                    } else {
+                        matrixDetails = confusionMatrix.get("spam");
+                        t0 = matrixDetails.get(0);
+                        matrixDetails.set(0, t0 + 1);
+                        confusionMatrix.put("spam",matrixDetails);
+                    }
+                } else{
+                    //not spam not spam
+                    if (! confusionMatrix.containsKey("nonSpam")) {
+                        matrixDetails.add(0.0);
+                        matrixDetails.add(1.0);
+                        confusionMatrix.put("nonSpam",matrixDetails);
+                    } else {
+                        matrixDetails = confusionMatrix.get("nonSpam");
+                        t1 = matrixDetails.get(1);
+                        matrixDetails.set(1,t1+1);
+                        confusionMatrix.put("nonSpam",matrixDetails);
+                    }
+
+                }
+            } else {
+                if (test.equals("spam")){
+                    // spam spam
+                    if (! confusionMatrix.containsKey("spam")) {
+                        matrixDetails.add(0.0);
+                        matrixDetails.add(1.0);
+                        confusionMatrix.put("spam",matrixDetails);
+                    } else {
+                        matrixDetails = confusionMatrix.get("spam");
+                        t1 = matrixDetails.get(1);
+                        matrixDetails.set(1, t1 + 1);
+                        confusionMatrix.put("spam",matrixDetails);
+                    }
+                } else{
+                    //not spam not spam
+                    if (! confusionMatrix.containsKey("nonSpam")) {
+                        matrixDetails.add(1.0);
+                        matrixDetails.add(0.0);
+                        confusionMatrix.put("nonSpam",matrixDetails);
+                    } else {
+                        matrixDetails = confusionMatrix.get("nonSpam");
+                        t0 = matrixDetails.get(0);
+                        matrixDetails.set(0,t0 + 1);
+                        confusionMatrix.put("nonSpam",matrixDetails);
+                    }
+
+                }
+            }
+        }
+        printConfusionMatrix();
+
+    }
+    private double calcSpamProb(int spam_m, int non_spam_m){
+        return  ( double) spam_m / (spam_m + non_spam_m);
+
+    }
+    private void printMaps(HashMap<String, Integer> ha){
+        for(Map.Entry<String, Integer> entry : ha.entrySet()){
+            System.out.printf("Key : %s :: %s %n", entry.getKey(), entry.getValue());
+        }
+    }
+    private void printMapsD(HashMap<String, Double> ha){
+        for(Map.Entry<String, Double> entry : ha.entrySet()){
+            System.out.printf("Key : %s :: %s %n", entry.getKey(), entry.getValue());
+        }
+    }
+    private void populateCounts(String[] str, Boolean docType){
+        String[] temp = new String[2];
+
+        int val = 0;
+        Boolean cFlag = Boolean.TRUE;
+        for (String st : str){
+            if (cFlag) {
+                cFlag = Boolean.FALSE;
+                continue;
+
+            }
+            temp = st.split(":");
+            totalUniqueWords.add(temp[0]);
+            if (docType){
+
+                if (  !spam_hash.containsKey(temp[0]) ) {
+                    spam_hash.put(temp[0],Integer.parseInt(temp[1]));
+                }else {
+                    val = spam_hash.get(temp[0]);
+                    spam_hash.put(temp[0], val+Integer.parseInt(temp[1]));
+                }
+            } else {
+                if (  !non_spam_hash.containsKey(temp[0])) {
+                    non_spam_hash.put(temp[0], Integer.parseInt(temp[1]));
+                }else {
+                    val = non_spam_hash.get(temp[0]);
+                    non_spam_hash.put(temp[0], val+Integer.parseInt(temp[1]));
+                }
+            }
+        }
+    }
+
+    private void readFile(String fileName) throws IOException {
+        BufferedReader  bufferedReader = new BufferedReader(new FileReader(fileName));
+        String in_line;
+        String[] temp ;
+        while((in_line = bufferedReader.readLine()) != null){
+            if (in_line.equals("")) continue;
+            temp = in_line.split(" ");
+//            System.out.println("LINE IS "+in_line);
+            if ( temp[0].equals("0") ) {
+                non_spam_message++;
+                populateCounts(temp, Boolean.FALSE);
+            }else{
+                spam_messages++;
+                populateCounts(temp, Boolean.TRUE);
+            }
+        }
+
+    }
+
+    public static  void main(String[] args) throws IOException {
+        SpamFilter sf = new SpamFilter("/Users/Sam/AI_MP/MP3/spam_detection/train_email.txt");
+//        SpamFilter sf = new SpamFilter("/Users/Sam/AI_MP/MP3/spam_detection/sample.txt");
+        String testFile = "/Users/Sam/AI_MP/MP3/spam_detection/test_email.txt";
+//        String testFile = "/Users/Sam/AI_MP/MP3/spam_detection/test_email_sample.txt";
+        sf.predictClassification(testFile);
+
+    }
+}
---
+++ MP3/src/main/java/TextDocClassification.java
@@ -0,0 +1,370 @@
+import java.io.*;
+import java.text.DecimalFormat;
+import java.util.*;
+
+/**
+ * Created by sam on 4/8/15.
+ */
+public class TextDocClassification {
+    public int LABEL_COUNT;
+    private  double LAPLACE;
+    private Integer totalTrainingDoc = 0;
+    private Set<String> totalUniqueWords = new HashSet<String>();
+    private double[][] confusionMatrix ;
+    private Integer totalTestDoc = 0 ;
+
+    private HashMap<Integer, Integer> labelRawCount = new HashMap<Integer, Integer>(); // to store total words
+    private HashMap<Integer, Integer> labelOccuranceCount = new HashMap<Integer, Integer>(); // to store total words
+    private HashMap<Integer, Integer> labelTestOccuranceCount = new HashMap<Integer, Integer>(); // to store total words
+    private HashMap<Integer, Double> labelOccuranceProb = new HashMap<Integer, Double>(); // to store total words
+    private HashMap<Integer, HashMap<String, Integer>> labelWordCount = new HashMap<Integer, HashMap<String , Integer>>();
+    private HashMap<Integer, HashMap<String, Double>> labelProbCount = new HashMap<Integer, HashMap<String , Double>>();
+    private HashMap<Integer, String[]> perClassTopLikelihoodWords = new HashMap<Integer, String[]>();
+    private HashMap<Integer, HashMap<String, Double>> wordsOddPerClass = new HashMap<Integer, HashMap<String, Double>>();
+    private int[] classIndex ;
+
+    public TextDocClassification(String fileName, int labelCount, int laplace) throws IOException {
+        LABEL_COUNT = labelCount;
+        LAPLACE = (double) laplace;
+        classIndex = new int[LABEL_COUNT];
+        confusionMatrix = new double[LABEL_COUNT][LABEL_COUNT];
+        readFile(fileName);
+        for(int labelType = 0 ;labelType < LABEL_COUNT; labelType++){
+            if (!labelWordCount.containsKey(labelType)) continue;
+            calcTotalWords(labelType);
+            labelProbCount.put(labelType,getProbCounts(labelType));
+        }
+    }
+
+
+
+    private void highestLikelihood() throws IOException {
+        HashMap<String, Double> ha = new HashMap<String, Double>();
+        for(Map.Entry<Integer, HashMap<String, Double>> entry : labelProbCount.entrySet()) {
+            File file1 = new File("/tmp/"+entry.getKey()+".csv");
+            file1.createNewFile();
+            FileWriter writer1 = new FileWriter(file1);
+            System.out.println("CLASS :"+entry.getKey());
+            ha = entry.getValue();
+            for(Map.Entry<String, Double> entry2 : ha.entrySet()) {
+                writer1.write(entry2.getKey());
+                writer1.write(",");
+                writer1.write(String.format( "%.10f", entry2.getValue() ));
+                writer1.write("\n");
+            }
+        }
+    }
+    private HashMap<String, Integer> getWordCounts(String[] str, Integer labelType){
+        HashMap<String , Integer> res  = new HashMap<String, Integer>();
+        String[] temp = new String[2];
+        ArrayList<String > lineWords = new ArrayList<String>();
+        if (! labelWordCount.containsKey(labelType)) {
+            labelWordCount.put(labelType,new HashMap<String, Integer>());
+        }
+        res = labelWordCount.get(labelType);
+        for (String st : str) {
+            temp = st.split(":");
+            String word = temp[0];
+            totalUniqueWords.add(word);
+            if (res.containsKey(word)) {
+                int t = res.get(word);
+                res.put(word, t + Integer.parseInt(temp[1]));
+            } else {
+                res.put(word, Integer.parseInt(temp[1]));
+            }
+
+        }
+        return res;
+    }
+
+    private void calcTotalWords(Integer labelType){
+        int totalWords=0;
+        HashMap<String , Integer> ha = labelWordCount.get(labelType);
+        for(Map.Entry<String, Integer> entry : ha.entrySet()) {
+            totalWords+=entry.getValue();
+        }
+        labelRawCount.put(labelType,totalWords);
+    }
+    private HashMap<String, Double> getProbCounts(Integer labelType) {
+        double tempWordProb = 0.0;
+        int totalWords = labelRawCount.get(labelType);
+        HashMap<String, Integer> wordCountHash;
+        HashMap<String, Double> res = new HashMap<String, Double>();
+        wordCountHash = labelWordCount.get(labelType);
+        totalWords += totalUniqueWords.size();
+        for (Map.Entry<String, Integer> entry : wordCountHash.entrySet()) {
+            tempWordProb = (double) (entry.getValue() + LAPLACE) / totalWords;
+            res.put(entry.getKey(), tempWordProb);
+        }
+        return res;
+    }
+
+    private void readFile(String fileName) throws  IOException {
+        BufferedReader  bufferedReader = new BufferedReader(new FileReader(fileName));
+        String in_line;
+        String[] temp ;
+        while((in_line = bufferedReader.readLine()) != null){
+            if (in_line.equals("")) continue;
+            totalTrainingDoc++;
+            temp = in_line.split(" ");
+            int labelType = Integer.parseInt(temp[0]);
+            if (!labelOccuranceCount.containsKey(labelType)){
+                labelOccuranceCount.put(labelType,1);
+            } else {
+                int t = labelOccuranceCount.get(labelType);
+                labelOccuranceCount.put(labelType,t + 1);
+            }
+            labelWordCount.put(labelType,getWordCounts(Arrays.copyOfRange(temp, 1, temp.length),labelType));
+        }
+    }
+
+    private void calcLabelProb(){
+        for (int i = 0 ;i < LABEL_COUNT ; i++ ){
+            if (!labelWordCount.containsKey(i)) continue;
+            double t = (double) labelOccuranceCount.get(i) / totalTrainingDoc;
+            labelOccuranceProb.put(i,t);
+        }
+    }
+
+    private void findOdds() throws IOException {
+        //odds(Fij=1, c1, c2) = P(Fij=1 | c1) / P(Fij=1 | c2).
+        int c1;
+        int c2;
+        double oddsProb ;
+        double p1;
+        double p2;
+        Double vl;
+
+        HashMap<String, Double> wordsProbC1 = new HashMap<String, Double>();
+        HashMap<String, Double> wordsProbC2 = new HashMap<String, Double>();
+//        for(int index = 0;index< classIndex.length;index++){
+
+        for(int index = 0;index< classIndex.length;index++){
+            HashMap<String, Double> wordOdds = new HashMap<String, Double>();
+            c1=index;
+            c2=classIndex[index];
+            wordsProbC1 =  labelProbCount.get(c1);
+            wordsProbC2 =  labelProbCount.get(c2);
+
+//            printClassForeGroundProb(pixelProbC1, "numerator", index);
+//            printClassForeGroundProb(pixelProbC2, "denominator", index);
+            for(Map.Entry<String, Double> entry: wordsProbC1.entrySet()) {
+
+                if(!wordsProbC2.containsKey(entry.getKey()))  continue;
+
+                // getting only words which are common among the two classes and finding max odd op
+                // 20 words with the highest log-odds ratio for that pair of classes.
+                p1=wordsProbC1.get(entry.getKey());
+                p2=wordsProbC2.get(entry.getKey());
+                oddsProb = p1/p2;
+                vl=Math.log(oddsProb);
+                wordOdds.put(entry.getKey(), vl);
+            }
+            wordsOddPerClass.put(c1,wordOdds);
+        }
+    }
+
+    private void printWordOddsPerClass() throws  IOException{
+
+        HashMap<String, Double> ha = new HashMap<String, Double>();
+        for(int i=0; i<LABEL_COUNT;i++){
+            System.out.println("CLASS :"+i);
+            File file1 = new File("/tmp/"+i+".csv");
+            file1.createNewFile();
+            FileWriter writer1 = new FileWriter(file1);
+            ha = wordsOddPerClass.get(i);
+            for(Map.Entry<String, Double> entry2 : ha.entrySet()) {
+                writer1.write(entry2.getKey());
+                writer1.write(",");
+                writer1.write(String.format( "%.10f", entry2.getValue() ));
+                writer1.write("\n");
+            }
+        }
+    }
+
+    private double max(double a, double b){
+        if (Double.compare(a,b) > 1){
+            return a;
+        } else {
+            return b;
+        }
+    }
+    private void findMaxOddPairs(){
+        double[] res= new double[LABEL_COUNT];
+        int index=0;
+        for(int i: classIndex){
+            res[index] = confusionMatrix[index][classIndex[index]];
+            index++;
+        }
+
+        Arrays.sort(res);
+        System.out.println(Arrays.toString(res));
+//        System.out.println(Arrays.toString(Arrays.copyOfRange(res, res.length,res.length-1)));
+//        ArrayUtils.reverse(int[] array)
+    }
+
+    private Double classificationProb(String[] st, Integer labelType){
+        int totalWords = labelRawCount.get(labelType);
+        String[] keyValuePair = new String[2];
+        int val;
+        double totalProduct ;
+        totalProduct = Math.log(labelOccuranceProb.get(labelType));
+        double wordProb = 0.0;
+        String key;
+        HashMap<String , Double> prob ;
+        prob = labelProbCount.get(labelType);
+        for (String str : st) {
+            keyValuePair = str.split(":");
+            key = keyValuePair[0];
+            val = Integer.parseInt(keyValuePair[1]);
+            if (!totalUniqueWords.contains(key)) continue;
+            if (!prob.containsKey(key)) {
+                wordProb = LAPLACE / ( totalWords + totalUniqueWords.size());
+            } else {
+                wordProb = prob.get(key);
+            }
+            totalProduct+= Math.log(wordProb)*val;
+        }
+        return  totalProduct;
+    }
+    private void predictClassification(String fileName) throws  IOException{
+        BufferedReader  bufferedReader = new BufferedReader(new FileReader(fileName));
+        String in_line;
+        String[] temp ;
+        while((in_line = bufferedReader.readLine()) != null){
+            ArrayList<Double> classificationProbs = new ArrayList<Double>(LABEL_COUNT);
+            if (in_line.equals("")) continue;
+            totalTestDoc++;
+            temp = in_line.split(" ");
+            int testLabelType = Integer.parseInt(temp[0]);
+            if (!labelTestOccuranceCount.containsKey(testLabelType)){
+                labelTestOccuranceCount.put(testLabelType,1);
+            } else {
+                int t = labelTestOccuranceCount.get(testLabelType);
+                labelTestOccuranceCount.put(testLabelType,t + 1);
+            }
+
+            for(int labelType = 0 ;labelType < LABEL_COUNT; labelType++){
+                if (!labelWordCount.containsKey(labelType)) continue;
+                Double t =  classificationProb(Arrays.copyOfRange(temp, 1, temp.length), labelType);
+                classificationProbs.add(t);
+            }
+            Double max = classificationProbs.get(0);
+            int index = 0;
+            int i=0;
+
+            for ( Double t : classificationProbs){
+                if ( t > max ) {
+                    index = i ;
+                    max = t;
+                }
+                i++;
+            }
+            confusionMatrix[testLabelType][index]++;
+//            System.out.println("TEST: "+testLabelType+" PREDICTION: "+index);
+        }
+    }
+
+    private  void printConfusionMatrix(boolean debug, int K){
+        Double accuracy = 0.0;
+        Double t ;
+        if (debug){
+            System.out.println("TOTAL TEST DOCUMENTS READ   " + totalTestDoc);
+            System.out.println("\n\n*** CONFUSION MATRIX ***\n");
+        }
+//        for(int i=0; i<LABEL_COUNT; i++){
+//            System.out.println("TOTAL TEST DOCUMENTS OF TYPE "+i+" READ : "+labelTestOccuranceCount.get(i));
+//        }
+        for (int i = 0 ;i < LABEL_COUNT; i++){
+            for (int j = 0 ; j < LABEL_COUNT ; j++){
+                t = confusionMatrix[i][j]/labelTestOccuranceCount.get(i)*100;
+                if (debug){
+                    System.out.format("%10.3f", t);
+                    System.out.print("%");
+                }
+                if ( i == j) {
+                    accuracy+=t;
+                }
+            }
+            if (debug){
+                System.out.println();
+            }
+
+        }
+        double a = accuracy/LABEL_COUNT;
+        if (debug){
+            System.out.println("--------------------------");
+            System.out.print("OVERALL ACCURACY :");
+            System.out.format("%5.3f", a);
+            System.out.println("%");
+            System.out.println("---------------------------");
+        } else {
+            System.out.print(K+" ,");
+            System.out.format("%5.3f", a);
+            System.out.println();
+        }
+
+    }
+
+    private int max(double[] probs,int skipIndex){
+        int maxIndex = -1;
+        double maxValue = -1.0;
+        for(int i=0; i<probs.length; i++){
+            if ( i == skipIndex) continue;
+            if(Double.compare(probs[i],maxValue) > 0){
+                maxIndex = i;
+                maxValue = probs[i];
+            }
+        }
+        return maxIndex;
+    }
+    private void findHighConfusion(){
+//        int[] classIndex = new int[10];
+        for(int i=0; i<confusionMatrix.length; i++){
+            classIndex[i]=max(confusionMatrix[i],i);
+        }
+        System.out.println(Arrays.toString(classIndex));
+//        return classIndex
+    }
+
+
+    public static  void main(String[] args) throws IOException {
+        TextDocClassification nf;
+        String testFile;
+        System.out.println();
+        System.out.println();
+
+        for (int i=1; i<=1; i++){
+            //////////                  EMAIL                            ///////////////////////////////
+//            nf = new TextDocClassification("/Users/Sam/AI_MP/MP3/spam_detection/train_email.txt",2, i);
+//            testFile = "/Users/Sam/AI_MP/MP3/spam_detection/test_email.txt";
+//            System.out.println();
+//            System.out.println();
+//            System.out.println("**********************************************");
+//            System.out.println("        EMAIL CLASSIFICATION                   ");
+//            System.out.println("**********************************************");
+
+
+
+            //////////////////////////     NEWS                                   ////////////
+            nf = new TextDocClassification("/Users/Sam/AI_MP/MP3/8category/8category.training.txt",8,i);
+            testFile = "/Users/Sam/AI_MP/MP3/8category/8category.testing.txt";
+            System.out.println();
+            System.out.println();
+            System.out.println("**********************************************");
+            System.out.println("        NEWS CLASSIFICATION                   ");
+            System.out.println("**********************************************");
+
+
+            nf.calcLabelProb();
+            nf.predictClassification(testFile);
+            nf.printConfusionMatrix(true, i);
+            nf.findHighConfusion();
+            nf.findOdds();
+            nf.findMaxOddPairs();
+//            nf.printWordOddsPerClass();
+//            nf.highestLikelihood();
+        }
+
+    }
+}
